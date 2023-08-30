8a7cb6db353f4ae6419632022aba4c1ee7cbf7a9
charly <carlosgarita3110@gmail.com>
2023-08-29 20:58:05 -0600

    feat(controller): add Controller class for TipoInstrumento presentation

---
+++ src/main/java/instrumentos/presentation/tipos/Controller.java
@@ -0,0 +1,36 @@
+package instrumentos.presentation.tipos;
+
+import instrumentos.logic.TipoInstrumento;
+
+import instrumentos.logic.Service;
+import instrumentos.logic.TipoInstrumento;
+
+import java.util.List;
+
+public class Controller{
+	public Controller(View view, Model model) {
+		model.init(Service.instance().search(new TipoInstrumento()));
+		this.view = view;
+		this.model = model;
+		view.setController(this);
+		view.setModel(model);
+	}
+	public void search(TipoInstrumento filter) throws  Exception{
+		List<TipoInstrumento> rows = Service.instance().search(filter);
+		if (rows.isEmpty()){
+			throw new Exception("NINGUN REGISTRO COINCIDE");
+		}
+		model.setList(rows);
+		model.setCurrent(new TipoInstrumento());
+		model.commit();
+	}
+	public void edit(int row){
+		TipoInstrumento e = model.getList().get(row);
+		try {
+			model.setCurrent(Service.instance().read(e));
+			model.commit();
+		} catch (Exception ex) {}
+	}
+	View view;
+	Model model;
+}
