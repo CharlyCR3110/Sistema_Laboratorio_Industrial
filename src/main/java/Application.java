90a9296ce1501ac04b65a01051c460b1a55f01ac
charly <carlosgarita3110@gmail.com>
2023-08-29 20:59:36 -0600

    feat: integrate TipoInstrumento presentation into Application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,7 +9,12 @@ public class Application {
 		window = new JFrame();
 		window.setContentPane(new JTabbedPane());
 
+		instrumentos.presentation.tipos.Model tiposModel= new instrumentos.presentation.tipos.Model();
+		instrumentos.presentation.tipos.View tiposView = new instrumentos.presentation.tipos.View();
+		tiposController = new instrumentos.presentation.tipos.Controller(tiposView,tiposModel);
 
+		// Se agrega el panel de tipos de instrumento al tabbed pane (la pestanita de arriba y su contenido)
+		window.getContentPane().add("Tipos de Instrumento",tiposView.getPanel());
 		window.setSize(900, 400);
 		window.setResizable(true);
 		window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -17,6 +22,6 @@ public class Application {
 		window.setTitle("SILAB: Sistema de Laboratorio Industrial");
 		window.setVisible(true);
 	}
-
+	public static instrumentos.presentation.tipos.Controller tiposController;
 	public static JFrame window;
 }
