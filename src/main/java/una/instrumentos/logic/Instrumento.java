1faf5cf154625f47b32e4b35e5516b6b1f8dae98
charly <carlosgarita3110@gmail.com>
2023-08-29 23:39:57 -0600

    feat: add 'Instrumento' class structure

---
+++ src/main/java/una/instrumentos/logic/Instrumento.java
@@ -0,0 +1,89 @@
+package una.instrumentos.logic;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Instrumento {
+	public Instrumento() {
+		this("","","",0,0,0);
+	}
+	public Instrumento(String serie, String descripcion, String tipo, Integer minimo, Integer maximo, Integer tolerancia) {
+		this.serie = serie;
+		this.descripcion = descripcion;
+		this.tipo = tipo;
+		this.minimo = minimo;
+		this.maximo = maximo;
+		this.tolerancia = tolerancia;
+	}
+
+	public String getSerie() {
+		return serie;
+	}
+
+	public void setSerie(String serie) {
+		this.serie = serie;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Integer getMinimo() {
+		return minimo;
+	}
+
+	public void setMinimo(Integer minimo) {
+		this.minimo = minimo;
+	}
+
+	public Integer getMaximo() {
+		return maximo;
+	}
+
+	public void setMaximo(Integer maximo) {
+		this.maximo = maximo;
+	}
+
+	public Integer getTolerancia() {
+		return tolerancia;
+	}
+
+	public void setTolerancia(Integer tolerancia) {
+		this.tolerancia = tolerancia;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Instrumento that = (Instrumento) o;
+		return Objects.equals(serie, that.serie) && Objects.equals(descripcion, that.descripcion) && Objects.equals(tipo, that.tipo) && Objects.equals(minimo, that.minimo) && Objects.equals(maximo, that.maximo) && Objects.equals(tolerancia, that.tolerancia);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(serie, descripcion, tipo, minimo, maximo, tolerancia);
+	}
+
+	String serie;
+	String descripcion;
+	String tipo;	// Combo box
+	Integer minimo;
+	Integer maximo;
+	Integer tolerancia;
+
+	//Lista de calibraciones
+//	List<Calibracion> calibraciones;
+}
