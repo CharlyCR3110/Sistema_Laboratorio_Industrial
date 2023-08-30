172024d35c040018abd101209d7e7488ee5ea718
charly <carlosgarita3110@gmail.com>
2023-08-30 12:29:09 -0600

    feat: integrate Acerca de presentation into Application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -15,13 +15,16 @@ public class Application {
 		window.setContentPane(new JTabbedPane());
 
 		// Se usa el nombre completo de las clases para evitar conflictos con otras clases que se llamen igual
+		// VIEWS
 		una.instrumentos.presentation.tipos.Model tiposModel= new una.instrumentos.presentation.tipos.Model();
 		una.instrumentos.presentation.tipos.View tiposView = new una.instrumentos.presentation.tipos.View();
 
 		una.instrumentos.presentation.instrumentos.Model instrumentosModel= new una.instrumentos.presentation.instrumentos.Model();
 		una.instrumentos.presentation.instrumentos.View instrumentosView = new una.instrumentos.presentation.instrumentos.View();
 
+		una.instrumentos.presentation.acercaDe.View acercaDeView = new una.instrumentos.presentation.acercaDe.View();
 
+		// CONTROLLERS
 		tiposController = new una.instrumentos.presentation.tipos.Controller(tiposView,tiposModel);
 		instrumentosController = new una.instrumentos.presentation.instrumentos.Controller(instrumentosView,instrumentosModel);
 
@@ -30,6 +33,8 @@ public class Application {
 		window.getContentPane().add("Tipos de Instrumento",tiposView.getPanel());
 		// Se agrega el panel de instrumentos al tabbed pane (la pestanita de arriba y su contenido)
 		window.getContentPane().add("Instrumentos",instrumentosView.getPanel());
+		// Se agrega el panel de acerca de al tabbed pane (la pestanita de arriba y su contenido)
+		window.getContentPane().add("Acerca de",acercaDeView.getPanel());
 		window.setSize(900, 400);
 		window.setResizable(true);
 		window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
