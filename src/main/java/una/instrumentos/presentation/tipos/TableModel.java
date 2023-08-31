5eccd14155c9972e9a00564de979b748c5b20096
charly <carlosgarita3110@gmail.com>
2023-08-30 20:30:36 -0600

    refactor: restructured TableModel class in tipos package for improved organization and readability

--- src/main/java/una/instrumentos/presentation/tipos/TableModel.java
+++ src/main/java/una/instrumentos/presentation/tipos/TableModel.java
@@ -5,54 +5,56 @@ import una.instrumentos.logic.TipoInstrumento;
 import javax.swing.table.AbstractTableModel;
 import java.util.List;
 
-public class TableModel extends AbstractTableModel implements javax.swing.table.TableModel {
-	// Constructor
-	public TableModel(int[] cols, List<TipoInstrumento> rows){
-		this.cols=cols;
-		this.rows=rows;
-		initColNames();
+public class TableModel extends AbstractTableModel {
+	public static final int CODIGO = 0;
+	public static final int NOMBRE = 1;
+	public static final int UNIDAD = 2;
+
+	private final int[] cols;
+	private final List<TipoInstrumento> rows;
+	private final String[] colNames = {"Codigo", "Nombre", "Unidad"};
+
+	public TableModel(int[] cols, List<TipoInstrumento> rows) {
+		this.cols = cols;
+		this.rows = rows;
 	}
-	// Metodos
+
 	@Override
 	public int getColumnCount() {
 		return cols.length;
 	}
+
 	@Override
-	public String getColumnName(int col){
+	public String getColumnName(int col) {
 		return colNames[cols[col]];
 	}
+
 	@Override
-	public Class<?> getColumnClass(int col){
-		switch (cols[col]){
-			default: return super.getColumnClass(col);
-		}
+	public Class<?> getColumnClass(int col) {
+		return getValueAt(0, col).getClass();
 	}
+
 	@Override
 	public int getRowCount() {
 		return rows.size();
 	}
+
 	@Override
 	public Object getValueAt(int row, int col) {
-		TipoInstrumento sucursal = rows.get(row);
-		switch (cols[col]){
-			case CODIGO: return sucursal.getCodigo();
-			case NOMBRE: return sucursal.getNombre();
-			case UNIDAD: return sucursal.getUnidad();
-			default: return "";
+		TipoInstrumento tipoInstrumento = rows.get(row);
+		switch (cols[col]) {
+			case CODIGO:
+				return tipoInstrumento.getCodigo();
+			case NOMBRE:
+				return tipoInstrumento.getNombre();
+			case UNIDAD:
+				return tipoInstrumento.getUnidad();
+			default:
+				return "";
 		}
 	}
+
 	public TipoInstrumento getRowAt(int row) {
 		return rows.get(row);
 	}
-	public static final int CODIGO = 0;
-	public static final int NOMBRE = 1;
-	public static final int UNIDAD = 2;
-	String[] colNames = new String[6];
-	private void initColNames(){
-		colNames[CODIGO] = "Codigo";
-		colNames[NOMBRE] = "Nombre";
-		colNames[UNIDAD] = "Unidad";
-	}
-	List<TipoInstrumento> rows;
-	int[] cols;
 }
