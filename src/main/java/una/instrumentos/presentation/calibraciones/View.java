9a0b915c4b31512573b9c93d5e8d90baa2ef32db
charly <carlosgarita3110@gmail.com>
2023-08-30 20:36:33 -0600

    refactor: restructured View class in calibraciones package for improved organization and readability

--- src/main/java/una/instrumentos/presentation/calibraciones/View.java
+++ src/main/java/una/instrumentos/presentation/calibraciones/View.java
@@ -6,7 +6,8 @@ import una.utiles.Utiles;
 
 import javax.swing.*;
 import javax.swing.table.TableColumnModel;
-import java.awt.event.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.time.LocalDate;
 import java.util.Observable;
 import java.util.Observer;
@@ -30,10 +31,13 @@ public class View implements Observer {
 	private JButton report;
 	private JLabel instrumentoLbl;
 
+	private Controller controller;
+	private Model model;
+	private Instrumento instrumentoSeleccionado;
+
 	public View() {
-		// Para que no se pueda editar la tabla
 		list.getTableHeader().setReorderingAllowed(false);
-		// Eventos
+
 		search.addActionListener(e -> searchAction());
 		list.addMouseListener(new MouseAdapter() {
 			@Override
@@ -66,68 +70,72 @@ public class View implements Observer {
 			}
 		});
 	}
+
 	private void editAction() {
 		try {
-			// Se obtiene la calibracion seleccionada desde la lista
 			Calibracion calibracion = model.getCurrent();
 			controller.edit(calibracion);
 			clearAction();
 		} catch (Exception ex) {
-			JOptionPane.showMessageDialog(panel, ex.getMessage(), "Información", JOptionPane.INFORMATION_MESSAGE);
+			showError(ex.getMessage());
 		}
 	}
+
 	private void deleteAction() {
 		try {
-			// Se obtiene la calibracion seleccionada desde la lista
 			Calibracion calibracion = model.getCurrent();
 			controller.delete(calibracion);
 			clearAction();
 		} catch (Exception ex) {
-			JOptionPane.showMessageDialog(panel, ex.getMessage(), "Información", JOptionPane.INFORMATION_MESSAGE);
+			showError(ex.getMessage());
 		}
 	}
+
 	private void saveAction() {
 		try {
 			Calibracion calibracion = new Calibracion();
 			calibracion.setNumero(numero.getText());
 			calibracion.setNumeroDeMediciones(Integer.valueOf(mediciones.getText()));
 			calibracion.setFecha(Utiles.parseDate(fecha.getText()));
+
 			if (instrumentoSeleccionado == null) {
 				showError("Debe seleccionar un instrumento");
 				return;
 			}
+
 			controller.save(calibracion, instrumentoSeleccionado);
 			clearAction();
 		} catch (Exception ex) {
-			JOptionPane.showMessageDialog(panel, "El formato de la fecha no es valido", "Información", JOptionPane.INFORMATION_MESSAGE);
+			showError("El formato de la fecha no es válido");
 		}
 	}
+
 	private void clearAction() {
-		// Se limpian los campos (o sea, se ponen en blanco)
 		numero.setText("");
 		mediciones.setText("0");
 		fecha.setText("0");
-		// Ademas de limpiar los campos, se deselecciona la lista
 		list.clearSelection();
-		// Reactivar el boton de guardar
 		save.setEnabled(true);
 		numero.setEnabled(true);
 	}
+
 	private void searchAction() {
 		try {
 			Calibracion filter = new Calibracion();
 			filter.setNumero(searchNumero.getText());
 			controller.search(filter);
 		} catch (Exception ex) {
-			JOptionPane.showMessageDialog(panel, ex.getMessage(), "Información", JOptionPane.INFORMATION_MESSAGE);
+			showError(ex.getMessage());
 		}
 	}
+
 	private void handleListClick() {
 		int row = list.getSelectedRow();
-		controller.edit(row);   // Se envia la fila seleccionada al controlador
+		controller.edit(row);
 	}
-	private void showErrorMessageBox(String message) {
-		JOptionPane.showMessageDialog(panel, message, "Información", JOptionPane.INFORMATION_MESSAGE);
+
+	public void showError(String message) {
+		JOptionPane.showMessageDialog(panel, message, "Error", JOptionPane.ERROR_MESSAGE);
 	}
 
 	public JPanel getPanel() {
@@ -148,36 +156,36 @@ public class View implements Observer {
 		int changedProps = (int) properties;
 		if ((changedProps & Model.LIST) == Model.LIST) {
 			int[] cols = {TableModel.NUMERO, TableModel.FECHA, TableModel.MEDICIONES};
-			System.out.println("LIST: " + model.getList().size());
 			list.setModel(new TableModel(cols, model.getList()));
-			System.out.println("DEBU1G");
-			for (Calibracion el : model.getList()) {
-				System.out.println("DEBUG");
-				System.out.println(el.getNumero());
-			}
-
 			list.setRowHeight(30);
 			TableColumnModel columnModel = list.getColumnModel();
 			columnModel.getColumn(2).setPreferredWidth(200);
 		}
 		if ((changedProps & Model.CURRENT) == Model.CURRENT) {
-			numero.setText(String.valueOf(model.getCurrent().getNumero()));
-			fecha.setText(model.getCurrent().getFecha().toString());
-			mediciones.setText(model.getCurrent().getNumeroDeMediciones().toString());
-			// Se deshabilitan el boton Guardar y el campo de codigo
-			if (model.getCurrent().getNumero().equals("") || model.getList().isEmpty()) {
-				save.setEnabled(true);
-				numero.setEnabled(true);
-			} else {
-				save.setEnabled(false);
-				numero.setEnabled(false);
-			}
+			Calibracion currentCalibracion = model.getCurrent();
+			numero.setText(String.valueOf(currentCalibracion.getNumero()));
+			fecha.setText(currentCalibracion.getFecha().toString());
+			mediciones.setText(String.valueOf(currentCalibracion.getNumeroDeMediciones()));
+
+			boolean enableEdit = currentCalibracion.getNumero().isEmpty() || model.getList().isEmpty();
+			save.setEnabled(enableEdit);
+			numero.setEnabled(enableEdit);
 		}
-		this.panel.revalidate();
+		panel.revalidate();
 	}
-	public void showError(String message) {
-		JOptionPane.showMessageDialog(panel, message, "Error", JOptionPane.ERROR_MESSAGE);
+
+	public void setInstrumentoSeleccionado(Instrumento instrumento) {
+		if (instrumento == null) {
+			instrumentoLbl.setText("No hay ningún instrumento seleccionado");
+			instrumentoSeleccionado = null;
+		} else {
+			String labelText = String.format("%s - %s (%s - %s)",
+					instrumento.getSerie(), instrumento.getDescripcion(), instrumento.getMinimo(), instrumento.getMaximo());
+			instrumentoLbl.setText(labelText);
+			instrumentoSeleccionado = instrumento;
+		}
 	}
+
 	public void highlightEmptyField(String fieldName) {
 		switch (fieldName) {
 			case "numero":
@@ -192,27 +200,15 @@ public class View implements Observer {
 		}
 	}
 
-	// metodos para obtener los valores de los campos de texto
 	public String getNumero() {
 		return numero.getText();
 	}
+
 	public String getMediciones() {
 		return mediciones.getText();
 	}
+
 	public String getFecha() {
 		return fecha.getText();
 	}
-	public void setInstrumentoSeleccionado(Instrumento instrumento) {
-		if (instrumento == null) {
-			this.instrumentoLbl.setText("No hay ningun instrumento seleccionado");
-			this.instrumentoSeleccionado = null;	// para evitar que se modifique otro instrumento
-			return;
-		}
-		this.instrumentoLbl.setText(String.format("%s - %s (%s - %s)", instrumento.getSerie(), instrumento.getDescripcion(), instrumento.getMinimo(), instrumento.getMaximo()));
-		instrumentoSeleccionado = instrumento;
-	}
-
-	Controller controller;
-	Model model;
-	Instrumento instrumentoSeleccionado;
 }
