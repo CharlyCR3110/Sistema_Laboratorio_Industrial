42f824813b587052618963f256461c107e29f801
charly <carlosgarita3110@gmail.com>
2023-08-30 20:23:58 -0600

    refactor: restructured Model class in tipos package for improved organization and readability

--- src/main/java/una/instrumentos/presentation/tipos/Model.java
+++ src/main/java/una/instrumentos/presentation/tipos/Model.java
@@ -1,45 +1,53 @@
 package una.instrumentos.presentation.tipos;
 
 import una.instrumentos.logic.TipoInstrumento;
-
 import java.util.List;
+import java.util.Observable;
 import java.util.Observer;
 
-public class Model extends java.util.Observable {
-	@Override
-	public void addObserver(Observer o) {	// Para que el modelo notifique a la vista de los cambios
+public class Model extends Observable {
+	private List<TipoInstrumento> list;
+	private TipoInstrumento current;
+	private int changedProps = NONE;
+
+	public static final int NONE = 0;
+	public static final int LIST = 1;
+	public static final int CURRENT = 2;
+
+	public void addObserver(Observer o) {
 		super.addObserver(o);
 		commit();
 	}
-	public void commit() {	// Para que el modelo notifique a la vista de los cambios
+
+	public void commit() {
 		setChanged();
 		notifyObservers(changedProps);
 		changedProps = NONE;
 	}
+
 	public Model() {
 	}
-	public void init(List<TipoInstrumento> list) {	// Inicializa el modelo con una lista de instrumentos
+
+	public void init(List<TipoInstrumento> list) {
 		setList(list);
 		setCurrent(new TipoInstrumento());
 	}
-	public List<TipoInstrumento> getList() {	// Devuelve la lista de instrumentos
+
+	public List<TipoInstrumento> getList() {
 		return list;
 	}
-	public void setList(List<TipoInstrumento> list) {	// Establece la lista de instrumentos
+
+	public void setList(List<TipoInstrumento> list) {
 		this.list = list;
-		changedProps += LIST;
+		changedProps |= LIST;
 	}
-	public TipoInstrumento getCurrent() {	// Devuelve el instrumento actual
+
+	public TipoInstrumento getCurrent() {
 		return current;
 	}
-	public void setCurrent(TipoInstrumento current) {	// Establece el instrumento actual
-		changedProps += CURRENT;
+
+	public void setCurrent(TipoInstrumento current) {
+		changedProps |= CURRENT;
 		this.current = current;
 	}
-	List<TipoInstrumento> list;	// Lista de instrumentos
-	TipoInstrumento current;	// Instrumento actual
-	int changedProps = NONE;	// Propiedades que han cambiado
-	public static int NONE = 0;	// Ninguna propiedad ha cambiado
-	public static int LIST = 1;	// La lista de instrumentos ha cambiado
-	public static int CURRENT = 2;	// El instrumento actual ha cambiado
 }
