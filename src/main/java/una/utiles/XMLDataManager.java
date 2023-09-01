91ae8af7c7e635fe6fd53cf64875f6b3bdbae5c0
charly <carlosgarita3110@gmail.com>
2023-08-31 22:12:24 -0600

    feat: add XMLDataManager for object serialization

--- src/main/java/una/utiles/XMLDataManager.java
+++ src/main/java/una/utiles/XMLDataManager.java
@@ -1,10 +1,12 @@
 package una.utiles;
 
+import una.instrumentos.logic.TipoInstrumento;
+
 import java.beans.XMLDecoder;
 import java.beans.XMLEncoder;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class XMLDataManager {
 	public static void saveToXML(Object data, String filePath) throws IOException {
@@ -12,9 +14,12 @@ public class XMLDataManager {
 			encoder.writeObject(data);
 		}
 	}
-	public static Object loadFromXML(String filePath) throws IOException {
-		try (XMLDecoder decoder = new XMLDecoder(new FileInputStream(filePath))) {
-			return decoder.readObject();
+	public static <T> List<T> loadFromXML(String filePath) {
+		try (XMLDecoder decoder = new XMLDecoder(new BufferedInputStream(new FileInputStream(filePath)))) {
+			return (List<T>) decoder.readObject();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
 		}
 	}
 }
