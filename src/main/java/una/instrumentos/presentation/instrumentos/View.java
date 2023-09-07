832b3c179e9a2c2fea8a95b9aeb82f1b6e1706b2
charly <carlosgarita3110@gmail.com>
2023-09-07 15:38:21 -0600

    refactor: remove the generateReport method and directly call controller.generateReport() from the mouse listener.

--- src/main/java/una/instrumentos/presentation/instrumentos/View.java
+++ src/main/java/una/instrumentos/presentation/instrumentos/View.java
@@ -83,7 +83,7 @@ public class View implements Observer {
 		report.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				generateReport();
+				controller.generateReport();
 			}
 		});
 		list.getSelectionModel().addListSelectionListener(e -> {
@@ -97,10 +97,6 @@ public class View implements Observer {
 		updateDeleteButtonState();
 		updateEditButtonState();
 		updateSaveState();
-	}
-
-	private void generateReport() {
-		controller.generateReport();
 	}
 
 	public void clearAction() {
