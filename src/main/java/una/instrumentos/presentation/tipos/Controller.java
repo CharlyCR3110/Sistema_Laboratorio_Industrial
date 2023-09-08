b3e1c894a60bcd50797496ca5f5c006eb51cf2ed
charly <carlosgarita3110@gmail.com>
2023-09-07 19:53:35 -0600

    chore: Add comments to various methods in the code

--- src/main/java/una/instrumentos/presentation/tipos/Controller.java
+++ src/main/java/una/instrumentos/presentation/tipos/Controller.java
@@ -11,6 +11,9 @@ import una.instrumentos.logic.Service;
 import java.io.FileOutputStream;
 import java.util.List;
 
+/**
+ * Controlador que maneja la lógica de la interfaz de usuario para la gestión de tipos de instrumentos.
+ */
 public class Controller {
 	private final View view;
 	private final Model model;
@@ -26,11 +29,18 @@ public class Controller {
 	}
 
 	private void initializeComponents() {
+		// Inicializa los componentes
 		model.init(Service.instance().search(new TipoInstrumento()));
 		view.setController(this);
 		view.setModel(model);
 	}
 
+
+	/**
+	 * Realiza una búsqueda de tipos de instrumentos basada en un filtro y actualiza el modelo.
+	 *
+	 * @param filter El filtro de búsqueda.
+	 */
 	public void search(TipoInstrumento filter) {
 		try {
 			List<TipoInstrumento> rows = Service.instance().search(filter);
@@ -46,10 +56,14 @@ public class Controller {
 	}
 
 	public void edit(int row) {
+		// Obtener el elemento seleccionado de la lista
 		TipoInstrumento e = model.getList().get(row);
 		try {
+			// Lee el elemento desde la base de datos (Data)
 			TipoInstrumento current = Service.instance().read(e);
+			// Setea el elemento seleccionado en el modelo
 			model.setCurrent(current);
+			// Se hace commit para que se actualice la vista
 			model.commit();
 		} catch (Exception ex) {
 			ex.printStackTrace();
@@ -57,15 +71,21 @@ public class Controller {
 	}
 
 	public void edit(TipoInstrumento e) {
+		// Se lee el elemento desde la base de datos (Data)
 		try {
 			TipoInstrumento current = Service.instance().read(e);
+
+			// Se setean los valores desde el view
 			current.setNombre(view.getNombre());
 			current.setUnidad(view.getUnidad());
 
+			// Se actualiza el elemento en la base de datos
 			Service.instance().update(current);
 			model.setCurrent(current);
 			model.commit();
-			search(new TipoInstrumento());	// Para refrescar la lista
+
+			// Se actualiza la vista
+			search(new TipoInstrumento());
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
@@ -178,6 +198,11 @@ public class Controller {
 		}
 	}
 
+	/**
+	 * Maneja la acción de eliminación de un tipo de instrumento seleccionado.
+	 *
+	 * @param selectedRow El índice de la fila seleccionada en la lista.
+	 */
 	public void handleDeleteAction(int selectedRow) {
 		if (selectedRow < 0) {
 			view.showError("Debe seleccionar un elemento de la lista");
@@ -192,6 +217,13 @@ public class Controller {
 		}
 	}
 
+	/**
+	 * Maneja la acción de guardar un nuevo tipo de instrumento.
+	 *
+	 * @param codigo El código del tipo de instrumento.
+	 * @param nombre El nombre del tipo de instrumento.
+	 * @param unidad La unidad del tipo de instrumento.
+	 */
 	public void handleSaveAction(String codigo, String nombre, String unidad) {
 		TipoInstrumento tipoInstrumento = new TipoInstrumento();
 		tipoInstrumento.setCodigo(codigo);
@@ -204,6 +236,11 @@ public class Controller {
 		}
 	}
 
+	/**
+	 * Maneja la acción de editar un tipo de instrumento seleccionado.
+	 *
+	 * @param selectedRow El índice de la fila seleccionada en la lista.
+	 */
 	public void handleEditAction(int selectedRow) {
 		if (selectedRow < 0) {
 			view.showError("Debe seleccionar un elemento de la lista");
@@ -218,6 +255,11 @@ public class Controller {
 		}
 	}
 
+	/**
+	 * Maneja la acción de búsqueda de tipos de instrumento por nombre.
+	 *
+	 * @param searchNombre El nombre a buscar.
+	 */
 	public void handleSearchAction(String searchNombre) {
 		TipoInstrumento tipoInstrumento = new TipoInstrumento();
 		tipoInstrumento.setNombre(searchNombre);
