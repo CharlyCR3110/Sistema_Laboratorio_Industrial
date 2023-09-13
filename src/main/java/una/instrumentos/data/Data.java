b0ba0437d0ef364a8911f98f2a6495a326f85302
charly <carlosgarita3110@gmail.com>
2023-09-12 20:37:14 -0600

    refactor: update Data class for XML serialization

--- src/main/java/una/instrumentos/data/Data.java
+++ src/main/java/una/instrumentos/data/Data.java
@@ -1,5 +1,6 @@
 package una.instrumentos.data;
 
+import jakarta.xml.bind.annotation.*;
 import una.instrumentos.logic.Calibracion;
 import una.instrumentos.logic.Instrumento;
 import una.instrumentos.logic.TipoInstrumento;
@@ -8,9 +9,17 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+// Clase que contiene los datos que se van a serializar
+@XmlRootElement(name = "RootElementData")
+@XmlAccessorType(XmlAccessType.FIELD)	// Para que se serialicen los atributos
 public class Data implements java.io.Serializable {
+	@XmlElementWrapper(name = "tipos")
+	@XmlElement(name = "tipo")
 	private List<TipoInstrumento> tipos;
+	@XmlElementWrapper(name = "instrumentos")
+	@XmlElement(name = "instrumento")
 	private List<Instrumento> instrumentos;
+	@XmlTransient	// No se serializa, ya que se serializa en la clase Instrumento, ademas, el uso de esta lista es solo para facilitar algunas cosas y ser consistente con la forma en que se esta trabajando
 	private List<Calibracion> calibraciones;
 
 	public Data() {
