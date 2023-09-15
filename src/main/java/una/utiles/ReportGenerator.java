fa83fd29efcc58d435cca7e4bea4972afc77cbf8
charly <carlosgarita3110@gmail.com>
2023-09-15 14:38:37 -0600

    feat: add generateInstrumentsReport method to ReportGenerator

--- src/main/java/una/utiles/ReportGenerator.java
+++ src/main/java/una/utiles/ReportGenerator.java
@@ -3,13 +3,57 @@ import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import una.instrumentos.logic.Calibracion;
-import una.instrumentos.presentation.calibraciones.Model;
+import una.instrumentos.logic.Instrumento;
 
 import java.io.FileOutputStream;
 import java.util.List;
 
 public class ReportGenerator {
-	public static void generateCalibrationsReport(Model model, String filePath) {
+
+	public static void generateInstrumentsReport(una.instrumentos.presentation.instrumentos.Model model, String filePath) {
+		Document document = new Document();
+
+		try {
+			PdfWriter.getInstance(document, new FileOutputStream(filePath));
+			document.open();
+
+			Paragraph title = new Paragraph("Reporte de Instrumentos");
+			title.setAlignment(Element.ALIGN_CENTER);
+			document.add(title);
+
+
+			if (model.getList().isEmpty() || model.getList() == null) {
+				Paragraph noData = new Paragraph("No hay instrumentos registrados");
+				noData.setAlignment(Element.ALIGN_CENTER);
+				document.add(noData);
+				document.close();
+				return;
+			}
+
+			PdfPTable table = new PdfPTable(5);
+			table.setWidthPercentage(100);
+			table.addCell("Serie");
+			table.addCell("Descripción");
+			table.addCell("Mínimo");
+			table.addCell("Máximo");
+			table.addCell("Tolerancia");
+
+			for (Instrumento instrumento : model.getList()) {
+				table.addCell(instrumento.getSerie());
+				table.addCell(instrumento.getDescripcion());
+				table.addCell(String.valueOf(instrumento.getMinimo()));
+				table.addCell(String.valueOf(instrumento.getMaximo()));
+				table.addCell(String.valueOf(instrumento.getTolerancia()));
+			}
+
+			document.add(table);
+			document.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void generateCalibrationsReport(una.instrumentos.presentation.calibraciones.Model model, String filePath) {
 		Document document = new Document();
 
 		try {
