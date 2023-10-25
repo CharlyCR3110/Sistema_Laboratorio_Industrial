a7ecd9ed06ae494343c5dcd78e3afbd8e5601cfb
charly <carlosgarita3110@gmail.com>
2023-10-25 16:40:37 -0600

    delete unnecessary index

--- src/main/java/una/SQL Scripts/02_CreateTipoInstrumentoTable.sql
+++ src/main/java/una/SQL Scripts/02_CreateTipoInstrumentoTable.sql
@@ -4,7 +4,3 @@ CREATE TABLE IF NOT EXISTS tipo_instrumentos (
   `nombre` VARCHAR(50) NOT NULL,
   `unidad` VARCHAR(30) NULL,
   PRIMARY KEY (`codigo`));
-
-
-# Para poder referenciar a la tabla tipo_instrumentos desde otras tablas
-CREATE INDEX idx_nombre ON tipo_instrumentos(nombre);
