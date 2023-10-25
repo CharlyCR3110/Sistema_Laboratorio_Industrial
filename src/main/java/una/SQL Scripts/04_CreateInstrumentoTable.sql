cc92cc2b92fb6dd1a8aa11b7daa1ac581ff97091
charly <carlosgarita3110@gmail.com>
2023-10-25 16:09:57 -0600

    feat: Create database tables for 'calibracion', 'instrumento', and 'calibracion_instrumento'
    Added SQL script to create new tables 'calibracion', 'instrumento', and 'calibracion_instrumento' in the 'laboratorio' database. The 'calibracion' table includes columns 'numero', 'fecha', 'numero_de_mediciones', and 'instrumento_serie' with appropriate data types and foreign key constraint referencing 'instrumento' table. The 'instrumento' table includes columns 'serie', 'descripcion', 'tipo', 'minimo', 'maximo', and 'tolerancia' with appropriate data types and foreign key constraint referencing 'tipo_instrumentos' table. The 'calibracion_instrumento' table establishes a many-to-many relationship between 'calibracion' and 'instrumento' with composite primary keys and foreign key constraints.

---
+++ src/main/java/una/SQL Scripts/04_CreateInstrumentoTable.sql
@@ -0,0 +1,28 @@
+USE `laboratorio`;
+
+CREATE TABLE IF NOT EXISTS instrumento (
+    serie VARCHAR(255) PRIMARY KEY NOT NULL,
+    descripcion VARCHAR(255) NOT NULL,
+    tipo VARCHAR(50) NOT NULL,
+    FOREIGN KEY (tipo) REFERENCES tipo_instrumentos(nombre),
+    minimo INT NOT NULL,
+    maximo INT NOT NULL,
+    tolerancia INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS calibracion (
+    numero INT AUTO_INCREMENT PRIMARY KEY,
+    fecha DATE NOT NULL,
+    numero_de_mediciones INT NOT NULL,
+    instrumento_serie VARCHAR(255) NOT NULL,
+    FOREIGN KEY (instrumento_serie) REFERENCES instrumento(serie)
+);
+
+# Esta tabla es para la relacion uno a muchos entre calibracion e instrumento, una calibracion tiene un instrumento y un instrumento puede tener muchas calibraciones
+CREATE TABLE IF NOT EXISTS calibracion_instrumento (
+    calibracion_numero INT NOT NULL,
+    instrumento_serie VARCHAR(255) NOT NULL,
+    PRIMARY KEY (calibracion_numero, instrumento_serie),
+    FOREIGN KEY (calibracion_numero) REFERENCES calibracion(numero),
+    FOREIGN KEY (instrumento_serie) REFERENCES instrumento(serie)
+);
