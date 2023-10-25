9997678f3850696ff58a2edf12c2f792a47aec2b
charly <carlosgarita3110@gmail.com>
2023-10-25 15:31:47 -0600

    refactor: Simplify TipoInstrumento class
    Unnecessary annotations and imports were removed.

--- src/main/java/una/instrumentos/logic/TipoInstrumento.java
+++ src/main/java/una/instrumentos/logic/TipoInstrumento.java
@@ -1,14 +1,8 @@
 package una.instrumentos.logic;
 
-import jakarta.xml.bind.annotation.XmlAccessType;
-import jakarta.xml.bind.annotation.XmlAccessorType;
-import jakarta.xml.bind.annotation.XmlID;
-
 import java.util.Objects;
 
-@XmlAccessorType(XmlAccessType.FIELD)
 public class TipoInstrumento {
-	@XmlID
 	String codigo;
 	String nombre;
 	String unidad;
