51f9eacac5801e787d14db5e5f0fdb959b9795eb
charly <carlosgarita3110@gmail.com>
2023-10-25 17:38:29 -0600

    refactor: Simplify Service class by removing unused methods
    Removed unused methods related to Instrumento operations as they are not being used in the application. The Service class is now focused solely on Calibracion operations, simplifying its structure.

--- src/main/java/una/instrumentos/logic/Service.java
+++ src/main/java/una/instrumentos/logic/Service.java
@@ -30,23 +30,11 @@ public class Service {
 			e.printStackTrace();
 		}
 	}
-
-	public void loadInstrumentoList(List<Instrumento> list ) throws Exception {
-		data.setInstrumentos(list);
-		data.setInstrumentos(data.getInstrumentos());
-	}
 	public void loadCalibracionList(List<Calibracion> calibracionList) {
 		data.setCalibraciones(calibracionList);
 		data.setCalibraciones(data.getCalibraciones());
 	}
 	// CREATE
-		// INSTRUMENTO
-	public void create(Instrumento e) throws Exception {
-		Instrumento result = data.getInstrumentos().stream()
-				.filter(i->i.getSerie().equals(e.getSerie())).findFirst().orElse(null);
-		if (result==null) data.getInstrumentos().add(e);
-		else throw new Exception("Instrumento ya existe");
-	}
 		// CALIBRACION
 	public void create(Calibracion e) throws Exception {
 		Calibracion result = data.getCalibraciones().stream()
@@ -55,13 +43,6 @@ public class Service {
 		else throw new Exception("Calibracion ya existe");
 	}
 	// READ
-		// Instrumento
-	public Instrumento read(Instrumento e ) throws  Exception {
-		Instrumento result = data.getInstrumentos().stream()
-				.filter(i->i.getSerie().equals(e.getSerie())).findFirst().orElse(null);
-		if (result!=null) return result;
-		else throw new Exception("Instrumento no existe");
-	}
 		// Calibracion
 	public Calibracion read(Calibracion e) throws Exception {
 		Calibracion result = data.getCalibraciones().stream()
@@ -70,14 +51,7 @@ public class Service {
 		else throw new Exception("Calibracion no existe");
 	}
 	// UPDATE
-		// Instrumento
-	public void update(Instrumento e) throws Exception {
-		if (e == null || !data.getInstrumentos().contains(e)) {
-			throw new Exception("Instrumento no existe");
-		}
-		// Si el instrumento existe, se actualiza la información
-		search(e).get(0).updateInfo(e);
-	}
+
 		// Calibracion
 	public void update (Calibracion e) throws Exception {
 		Calibracion result;
@@ -90,26 +64,11 @@ public class Service {
 		}
 	}
 	// DELETE
-		// Instrumento
-	public void delete(Instrumento e) throws Exception {
-		// Verificar que no existan calibraciones con ese instrumento
-		if (e.hasCalibraciones()) {
-			throw new Exception("Parece que hay calibraciones asociadas a este instrumento");
-		}
-		data.getInstrumentos().remove(e);
-	}
 		// Calibracion
 	public void delete(Calibracion e) throws Exception {
 		data.getCalibraciones().remove(e);
 	}
 	// SEARCH
-		// Instrumento
-	public List<Instrumento> search(Instrumento e){
-		return data.getInstrumentos().stream()
-				.filter(i->i.getDescripcion().contains(e.getDescripcion()))
-				.sorted(Comparator.comparing(Instrumento::getDescripcion))
-				.collect(Collectors.toList());
-	}
 		// Calibracion
 	public List<Calibracion> search(Calibracion e) {
 		// si el numero de calibracion es null, entonces se devuelve la lista completa
