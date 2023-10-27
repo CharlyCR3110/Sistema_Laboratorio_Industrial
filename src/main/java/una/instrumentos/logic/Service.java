e88e33912049b6fb7d3850bd0ff2372a8edb38de
charly <carlosgarita3110@gmail.com>
2023-10-27 10:58:11 -0600

    refactor: Simplify Service class by removing unused methods
    Removed unused methods related to Calibracion operations as they are not being used in the application.

--- src/main/java/una/instrumentos/logic/Service.java
+++ src/main/java/una/instrumentos/logic/Service.java
@@ -9,77 +9,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Service {
+	private static Service theInstance;
 	public static Service instance(){
 		if (theInstance == null) theInstance = new Service();
 		return theInstance;
 	}
-	private Data data;
-
 	private Service(){
-		try {
-			data = XmlPersister.instance().load();
-		} catch (Exception e) {
-			data = new Data();
-		}
 	}
-
-	public void stop() {
-		try {
-			XmlPersister.instance().store(data);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	public void loadCalibracionList(List<Calibracion> calibracionList) {
-		data.setCalibraciones(calibracionList);
-		data.setCalibraciones(data.getCalibraciones());
-	}
-	// CREATE
-		// CALIBRACION
-	public void create(Calibracion e) throws Exception {
-		Calibracion result = data.getCalibraciones().stream()
-				.filter(i->i.getNumero().equals(e.getNumero())).findFirst().orElse(null);
-		if (result==null) data.getCalibraciones().add(e);
-		else throw new Exception("Calibracion ya existe");
-	}
-	// READ
-		// Calibracion
-	public Calibracion read(Calibracion e) throws Exception {
-		Calibracion result = data.getCalibraciones().stream()
-				.filter(i->i.getNumero().equals(e.getNumero())).findFirst().orElse(null);
-		if (result!=null) return result;
-		else throw new Exception("Calibracion no existe");
-	}
-	// UPDATE
-
-		// Calibracion
-	public void update (Calibracion e) throws Exception {
-		Calibracion result;
-		try{
-			result = this.read(e);
-			data.getCalibraciones().remove(result);
-			data.getCalibraciones().add(e);
-		}catch (Exception ex) {
-			throw new Exception("Calibracion no existe");
-		}
-	}
-	// DELETE
-		// Calibracion
-	public void delete(Calibracion e) throws Exception {
-		data.getCalibraciones().remove(e);
-	}
-	// SEARCH
-		// Calibracion
-	public List<Calibracion> search(Calibracion e) {
-		// si el numero de calibracion es null, entonces se devuelve la lista completa
-		return e.equals(new Calibracion()) ? data.getCalibraciones() : data.getCalibraciones().stream()
-				.filter(i->i.getNumero().equals(e.getNumero()))
-				.sorted(Comparator.comparing(Calibracion::getNumero))
-				.collect(Collectors.toList());
-	}
-	private static Service theInstance;
-
-
 	public Service getInstance() {
 		return theInstance;
 	}
