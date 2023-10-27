747a4518a6a0a892122cc7efcaad3ee8803aaa2c
charly <carlosgarita3110@gmail.com>
2023-10-27 10:56:39 -0600

    refactor(database schema): remove redundant tables and optimize relationships

--- src/main/java/una/SQL Scripts/02_CreateTables.sql
+++ src/main/java/una/SQL Scripts/02_CreateTables.sql
@@ -1,43 +1,33 @@
 USE `laboratorio`;
 
 CREATE TABLE IF NOT EXISTS tipo_instrumentos (
-    codigo VARCHAR(20) NOT NULL,
+    codigo VARCHAR(20) NOT NULL PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
-    unidad VARCHAR(30) NULL,
-    PRIMARY KEY (`codigo`)
-);
-
-CREATE TABLE IF NOT EXISTS mediciones (
-    numero INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    referencia INT NOT NULL,
-    medicion INT NOT NULL
+    unidad VARCHAR(30) NULL
 );
 
 CREATE TABLE IF NOT EXISTS instrumentos (
     serie VARCHAR(255) PRIMARY KEY NOT NULL,
     descripcion VARCHAR(255) NOT NULL,
     tipo VARCHAR(20) NOT NULL,
-    FOREIGN KEY (tipo) REFERENCES tipo_instrumentos(codigo),
     minimo INT NOT NULL,
     maximo INT NOT NULL,
-    tolerancia INT NOT NULL
+    tolerancia INT NOT NULL,
+    FOREIGN KEY (tipo) REFERENCES tipo_instrumentos(codigo)
 );
 
 CREATE TABLE IF NOT EXISTS calibraciones (
     numero INT AUTO_INCREMENT PRIMARY KEY,
     fecha DATE NOT NULL,
     numero_de_mediciones INT NOT NULL,
-    mediciones INT NOT NULL,
-    FOREIGN KEY (mediciones) REFERENCES mediciones(numero),
     instrumento_serie VARCHAR(255) NOT NULL,
     FOREIGN KEY (instrumento_serie) REFERENCES instrumentos(serie)
 );
 
-# Esta tabla es para la relacion uno a muchos entre calibracion e instrumento, una calibracion tiene un instrumento y un instrumento puede tener muchas calibraciones
-CREATE TABLE IF NOT EXISTS calibraciones_instrumento (
+CREATE TABLE IF NOT EXISTS mediciones (
+    numero INT AUTO_INCREMENT PRIMARY KEY,
+    referencia INT NOT NULL,
+    medicion INT NOT NULL,
     calibracion_numero INT NOT NULL,
-    instrumento_serie VARCHAR(255) NOT NULL,
-    PRIMARY KEY (calibracion_numero, instrumento_serie),
-    FOREIGN KEY (calibracion_numero) REFERENCES calibraciones(numero),
-    FOREIGN KEY (instrumento_serie) REFERENCES instrumentos(serie)
+    FOREIGN KEY (calibracion_numero) REFERENCES calibraciones(numero) ON DELETE CASCADE ON UPDATE CASCADE
 );
