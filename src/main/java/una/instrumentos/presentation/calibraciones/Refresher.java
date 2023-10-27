3b1c152b38c910e4407702e5ee41da9b8ba04541
charly <carlosgarita3110@gmail.com>
2023-10-27 15:00:35 -0600

    feat: add Refresher class

---
+++ src/main/java/una/instrumentos/presentation/calibraciones/Refresher.java
@@ -0,0 +1,43 @@
+package una.instrumentos.presentation.calibraciones;
+
+import una.instrumentos.presentation.calibraciones.Controller;
+
+import javax.swing.*;
+
+public class Refresher {
+
+	una.instrumentos.presentation.calibraciones.Controller controller;
+
+	public Refresher(Controller controller) {
+		this.controller = controller;
+	}
+
+	private Thread hilo;
+	private boolean condition=false;
+	public void start(){
+		Runnable task = new Runnable(){
+			public void run(){
+				while(condition){
+					try { Thread.sleep(5000);}
+					catch (InterruptedException ex) {}
+					refresh();
+				}
+			}
+		};
+		hilo = new Thread(task);
+		condition=true;
+		hilo.start();
+	}
+	public void stop(){
+		condition=false;
+	}
+	private void refresh(){
+		SwingUtilities.invokeLater(
+				new Runnable(){
+					public void run(){
+						controller.refresh();
+					}
+				}
+		);
+	}
+}
